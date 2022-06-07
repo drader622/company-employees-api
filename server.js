9e503e70769f09cd0bf01c02c815e32188c9d5a1
drader622 <drader0827@gmail.com>
2022-06-07 17:36:11 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = 8000;
+
+app.use(cors());
+
+const employees = {
+    '1596182': {
+        'firstName': 'Danielle',
+        'lastName': 'Rader',
+        'department': 'engineering',
+        'supervisor': 'Ted Bonham',
+        'position': 'Electronic Technician',
+        'Salary': '48.61/hr',
+    },
+    'unknown': {
+        'firstName': 'unknown',
+        'lastName': 'unknown',
+        'department': 'unknown',
+        'supervisor': 'unknown',
+        'position': 'unknown',
+        'Salary': 'unknown',
+    }
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/api/:id', (req, res) => {
+    const employeeID = req.params.id
+    if (employees[employeeID]) {
+        res.json(employees[employeeID])
+    } else {
+        res.json(employees['unknown']);
+    }
+});
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`Running on port ${PORT}`)
+});
